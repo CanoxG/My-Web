d636cdcb99c855fcd01f1ea6b32f06f952c170b8
Can Guven <bcanguven@gmail.com>
2021-12-24 11:14:11 +0000

    screen-img

--- components/post/Process.js
+++ components/post/Process.js
@@ -5,14 +5,14 @@ export default function Process({ children }) {
   return (
     <div
       className="brightness-75 h-96 bg-cover bg-center bg-no-repeat text-white"
-      style={{ backgroundImage: 'url("/images/process.jpeg")' }}
+      style={{ backgroundImage: 'url("/images/screen.jpeg")' }}
     >
       <Section>
-        <h2 className="pt-4 bg-white/90 text-center font-black text-lg sm:text-xl text-black">
+        <h2 className="pt-4 bg-white/70 text-center font-black text-lg sm:text-xl text-black">
           Project Design & Development Process
         </h2>
         <div
-          className="grid grid-cols-3 gap-2 px-2 pt-4 bg-white/90 rounded items-end
+          className="grid grid-cols-3 gap-2 px-2 pt-4 bg-white/70 rounded items-end
         sm:grid-cols-3 sm:gap-16 md:grid-cols-6 md:gap-1"
         >
           {children}
