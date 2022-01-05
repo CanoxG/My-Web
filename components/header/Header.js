3ccff0da328951cd9ac70a18d5e1ddf65981a2f5
Can Guven <bcanguven@gmail.com>
2022-01-05 10:04:45 +0000

    update

--- components/header/Header.js
+++ components/header/Header.js
@@ -14,9 +14,7 @@ export default function Header({ children }) {
         quality={100}
       />
       <div className="container mx-auto -mt-24 relative z-10">
-        <div className="flex sm:justify-end justify-center">
-          {children}
-        </div>
+        <div className="flex sm:justify-end justify-center">{children}</div>
       </div>
     </header>
   );
