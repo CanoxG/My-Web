5d0e5621ccc0d9210f354104b6b18ac067125666
Can Guven <bcanguven@gmail.com>
2022-01-06 16:47:22 +0000

    blur update

--- components/header/Header.js
+++ components/header/Header.js
@@ -3,15 +3,16 @@ import Image from "next/image";
 
 export default function Header({ children }) {
   return (
-    <header className='mb-12'>
+    <header className="mb-12">
       <Image
         src={"/images/background.jpeg"}
         layout="responsive"
         width="100%"
         height="100%"
         objectFit={"cover"}
+        placeholder={"blur"}
+        blurDataURL={"/images/background.jpeg"}
         objectPosition={"center"}
-
         quality={100}
       />
       <div className="container mx-auto -mt-24 relative z-10">
