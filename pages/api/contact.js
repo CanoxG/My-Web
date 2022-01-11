7d2bdd209f4feb3c71334d9a8cb85ac88d5bae8b
Can Guven <bcanguven@gmail.com>
2022-01-11 21:44:06 +0000

    testing

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,28 +1,25 @@
-import nodeMailer from 'nodemailer';
+import nodeMailer from "nodemailer";
 
 
 // Assign a Variable to Our Password & E-mail
 
+export default async function contact(req, res) {
 
-export default async function getServerSideProps(req, res) {
-  console.log({ PASSWORD, TO_EMAIl, FROM_EMAIL })
-
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const transporter = nodeMailer.createTransport({
       // Transporter Stores the Info of SMTP Server and Sender Account
       host: "smtp.gmail.com",
       secure: true,
-
       auth: {
-        user: process.env.FROM_EMAIL ,
-        pass: process.env.PASSWORD ,
+        user: process.env.FROM_EMAIL,
+        pass: process.env.PASSWORD,
       },
     });
 
     // We Create a Mail Model
     const mailData = {
-      from: process.env.FROM_EMAIL ,
-      to: process.env.TO_EMAIL ,
+      from: process.env.FROM_EMAIL,
+      to: process.env.TO_EMAIL,
       subject: `Message From ${req.body.name}`,
       text: req.body.message + " | Sent from: " + req.body.email,
       html: `<div>${req.body.message}</div><p>Sent from:
@@ -43,6 +40,7 @@ export default async function getServerSideProps(req, res) {
   }
 
   return res.status(200).json({
-    status: 200, message: "no data"
-  })
+    status: 200,
+    message: "no data",
+  });
 }
