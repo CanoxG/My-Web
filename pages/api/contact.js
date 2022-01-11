710196ba7cda691325a808fe84d5fb6f7f402cc3
Can Guven <bcanguven@gmail.com>
2022-01-11 18:04:10 +0000

    fix domain

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,16 +1,15 @@
 import nodeMailer from 'nodemailer';
-import Cors from 'cors'
-import initMiddleware from '../../lib/init-middleware'
-import log from "tailwindcss/lib/util/log";
+// import Cors from 'cors'
+// import initMiddleware from '../../lib/init-middleware'
 
 // Initialize the cors middleware
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
-)
+// const cors = initMiddleware(
+//   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+//   Cors({
+//     // Only allow requests with GET, POST and OPTIONS
+//     methods: ['GET', 'POST', 'OPTIONS'],
+//   })
+// )
 
 // Assign a Variable to Our Password & E-mail
 const PASSWORD = process.env.password;
@@ -18,8 +17,13 @@ const TO_EMAIl = process.env.TO_EMAIL;
 const FROM_EMAIL = process.env.FROM_EMAIL;
 
 export default async function contact(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
   // Run cors
-  await cors(req, res);
+  // await cors(req, res);
   console.log({ PASSWORD, TO_EMAIl, FROM_EMAIL })
   if (req.method === 'POST') {
     const transporter = nodeMailer.createTransport({
