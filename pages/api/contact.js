14ad6e7b99d20d3d32d92520a32f04b51e5942bd
Can Guven <bcanguven@gmail.com>
2022-01-11 20:19:18 +0000

    fix domain

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,14 +2,9 @@ import nodeMailer from 'nodemailer';
 
 
 // Assign a Variable to Our Password & E-mail
-const PASSWORD = process.env.PASSWORD || 'TanoxCanox1903';
-const TO_EMAIl = process.env.TO_EMAIL || 'bcanguven@gmail.com';
-const FROM_EMAIL = process.env.FROM_EMAIL || 'guvenbcan@gmail.com';
 
-export default async function contact(req, res) {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "*");
-  // res.setHeader("Access-Control-Allow-Headers", "*");
+
+export default async function getServerSideProps(req, res) {
   console.log({ PASSWORD, TO_EMAIl, FROM_EMAIL })
 
   if (req.method === 'POST') {
@@ -19,15 +14,15 @@ export default async function contact(req, res) {
       secure: true,
 
       auth: {
-        user: FROM_EMAIL,
-        pass: PASSWORD,
+        user: process.env.FROM_EMAIL ,
+        pass: process.env.PASSWORD ,
       },
     });
 
     // We Create a Mail Model
     const mailData = {
-      from: FROM_EMAIL,
-      to: TO_EMAIl,
+      from: process.env.FROM_EMAIL ,
+      to: process.env.TO_EMAIL ,
       subject: `Message From ${req.body.name}`,
       text: req.body.message + " | Sent from: " + req.body.email,
       html: `<div>${req.body.message}</div><p>Sent from:
