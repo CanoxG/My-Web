183f98627ea4c90ceefa03578756056f38d8bec2
Can Guven <bcanguven@gmail.com>
2022-02-27 18:27:33 +0000

    text-size

--- components/head_line/H4.js
+++ components/head_line/H4.js
@@ -1,5 +1,5 @@
 export default function H4({ children }) {
-  return <h4 className="text-black text-md text-center lg:text-lg font-black mb-3">
+  return <h4 className="text-black text-sm text-center lg:text-lg font-black mb-3">
     {children}
   </h4>
 }
